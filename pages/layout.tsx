7088ab2f792f90e6fdd54e043d99d1e9156446b6
James Fincher <heartandkarma@gmail.com>
2022-11-08 19:36:04 -0800

    ...

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import Nav from "./components/layout/Nav/Nav";
 import Footer from "./components/layout/Footer";
 
-const Layout = ({ children }: { children: any }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Nav />
